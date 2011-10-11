b1a6e9d0bd758c743a0a3bccd9a9f512002d859f
bloomlydia <lydia@bloom.io>
2011-10-11 13:13:52 -0700

    just added a stub file so it's not missing.

---
+++ include/EffectManager.h
@@ -0,0 +1,22 @@
+//
+//  EffectManager.h
+//
+//  Copyright 2011 Bloom Studio, Inc. All rights reserved.
+//
+
+#pragma once
+#include "BloomScene.h"
+
+class EffectManager : public BloomScene {
+
+public:
+    EffectManager() {}
+    ~EffectManager() {}
+    
+    //STUB
+    
+private:
+    //STUB
+};
+
+typedef std::shared_ptr<EffectManager> EffectManagerRef;
