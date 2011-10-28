339f272335a38493f27030bbc07d011b0d5e31ad
bloomlydia <lydia@bloom.io>
2011-10-28 16:10:30 -0700

    better shader

--- samples/ParticleTest/resources/Particle_frag.glsl
+++ samples/ParticleTest/resources/Particle_frag.glsl
@@ -2,27 +2,16 @@ precision highp float;
 
 uniform sampler2D u_diffuseTex;
 uniform float u_tileWidth;
-uniform bool u_tileBlend;
 
-varying float v_tileIndex;
+varying float v_tileMix;
 varying vec4 v_color;
-varying vec2 v_texcoord;
+varying vec2 v_texcoordA;
+varying vec2 v_texcoordB;
 
 void main()
 {
-    float tIndex = floor(v_tileIndex);
-    
-    float tMix = 0.0;
-    
-    if (u_tileBlend)
-    {
-        tMix = v_tileIndex - tIndex;
-    }
-    
-    float uOffset = u_tileWidth * tIndex;
-    
-    vec4 colorA = texture2D(u_diffuseTex, vec2(v_texcoord.x * u_tileWidth + uOffset, v_texcoord.y));
-    vec4 colorB = texture2D(u_diffuseTex, vec2((v_texcoord.x + 1.0) * u_tileWidth + uOffset, v_texcoord.y));
+    vec4 colorA = texture2D(u_diffuseTex, v_texcoordA);
+    vec4 colorB = texture2D(u_diffuseTex, v_texcoordB);
 
-    gl_FragColor = mix(colorA, colorB, tMix) * v_color;
+    gl_FragColor = mix(colorA, colorB, v_tileMix) * v_color;
 }
